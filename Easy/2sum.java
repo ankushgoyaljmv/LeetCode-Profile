72009b46d9bf92f3b0b07ace81261842488aedaa
Ankush Goyal <54682973+ashgrininja@users.noreply.github.com>
2020-03-30 23:43:15 +0530

    to be optimized

--- Easy/2sum.java
+++ Easy/2sum.java
@@ -1,1 +1,23 @@
+//Problem:
+//https://leetcode.com/problems/two-sum
 
+
+class Solution {
+    public int[] twoSum(int[] nums, int target)
+    {   int result[]=new int[2];
+        for(int i=0;i<nums.length;i++)
+        {
+            for(int j=i+1;j<nums.length;j++)
+            {
+                if(nums[i]+nums[j]==target)
+                {     
+                    result[0]=i;
+                    result[1]=j;
+                    return result;
+                }
+            }
+        }
+     return new int[0];
+    }
+    
+}
