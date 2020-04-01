9440e2762ec34f72d3d81c3756bda632ee24409a
Ankush Goyal <54682973+ashgrininja@users.noreply.github.com>
2020-04-01 23:59:24 +0530

    Will Update

--- Easy/NumberofStepstoReduceaNumbertoZero.java
+++ Easy/NumberofStepstoReduceaNumbertoZero.java
@@ -1,5 +1,5 @@
-//Problem:
-//https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/submissions/
+//Problem:1342
+//https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/submissions
 
 import java.util.*;
 
