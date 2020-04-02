4fe07c3d907373b82856e3a457d0b4d5323b4db8
Ankush Goyal <54682973+ashgrininja@users.noreply.github.com>
2020-04-03 00:28:00 +0530

    Code Updated

--- Easy/CountPrimes(204).java
+++ Easy/CountPrimes(204).java
@@ -1,25 +1,59 @@
 //Problem:204
 //https://leetcode.com/problems/count-primes/
+
+
 class Solution {
     public int countPrimes(int n) {
-        int count = 0;
+             //base case: exclusive
+        if (n <= 2)
+            return 0;
 
-        int num = 2;
+        //already one prime encountered:2
+        int count = 1;
+        int num = 3;
+        
         //Numbers less than n(exclusive)
+        
         while (num < n) {
-                  //assuming every number to be prime
-                  boolean isprime = true;
-            for (int i = 2; i * i <= num; i++) {
-                if (num % i ==0) {
+            //assuming every number to be prime
+            boolean isprime = true;
+            for (int i = 2; i * i <=num; i++) {
+                if (num % i == 0) {
                     isprime = false;
                     break;
                 }
             }
-             if (isprime == true)
-                    count++;
-                num++;
+            if (isprime == true)
+                count++;
+            num += 2;
 
         }
         return count;
     }
 }
+
+
+/**************************************************************************************************/
+// class Solution {
+//     public int countPrimes(int n) {
+//         int count = 0;
+
+//         int num = 2;
+//         //Numbers less than n(exclusive)
+//         while (num < n) {
+//                   //assuming every number to be prime
+//                   boolean isprime = true;
+//             for (int i = 2; i * i <= num; i++) {
+//                 if (num % i ==0) {
+//                     isprime = false;
+//                     break;
+//                 }
+//             }
+//              if (isprime == true)
+//                     count++;
+//                 num++;
+
+//         }
+//         return count;
+//     }
+// }
