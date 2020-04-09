db6ef10c312362f89847f9e8834945b342d050b2
Ankush Goyal <54682973+ashgrininja@users.noreply.github.com>
2020-04-10 00:19:00 +0530

    Version1

--- Easy/(9)PalindromeNumber.java
+++ Easy/(9)PalindromeNumber.java
@@ -4,18 +4,19 @@
 class Solution {
     public boolean isPalindrome(int x) {
         
-        
+    
         int rev=0;
         int temp=x;
                 
         while(x!=0)
         {
             int rem=x%10;
-            rev+=rev*10+rem;
+            rev =rev*10+rem;
             x/=10;
             
         }
-        if(rev==temp)
+
+        if(temp>=0 && temp==rev)
             return true; 
         else
             return false;
