c47768f17569a6bcc8a90417a0b36713b815a90e
Ankush Goyal <54682973+ashgrininja@users.noreply.github.com>
2020-04-10 00:22:08 +0530

    Optimized Code with 2 Variations

--- Easy/(9)PalindromeNumber.java
+++ Easy/(9)PalindromeNumber.java
@@ -2,8 +2,12 @@
 //https://leetcode.com/problems/palindrome-number
 
 class Solution {
-    public boolean isPalindrome(int x) {
-        
+    
+ //****************Version 1://////********
+    
+     public boolean isPalindrome(int x) {
+         if(x<0)
+             return false;
     
         int rev=0;
         int temp=x;
@@ -16,10 +20,35 @@ class Solution {
             
         }
 
-        if(temp>=0 && temp==rev)
+        if(temp==rev)
             return true; 
         else
             return false;
        
     }
+    
+    
+   
+//*************Method 2******************//    
+    
+//     public boolean isPalindrome(int x) {
+        
+    
+//         int rev=0;
+//         int temp=x;
+                
+//         while(x!=0)
+//         {
+//             int rem=x%10;
+//             rev =rev*10+rem;
+//             x/=10;
+            
+//         }
+
+//         if(temp>=0 && temp==rev)
+//             return true; 
+//         else
+//             return false;
+       
+//     }
 }
