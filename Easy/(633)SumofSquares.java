1e4053fc47cb52c7e281d026e812e317843b830b
Ankush Goyal <54682973+ashgrininja@users.noreply.github.com>
2020-04-10 23:56:06 +0530

    to be Viewed

--- Easy/(633)SumofSquares.java
+++ Easy/(633)SumofSquares.java
@@ -2,27 +2,51 @@
 //https://leetcode.com/problems/sum-of-square-numbers
 
 class Solution {
-    public boolean judgeSquareSum(int c)
+	
+	/******************Method 1*****************/
+	 public boolean judgeSquareSum(int c)
     {
-		
+        if(c==0||c==1)
+            return true;
+		//Similar to Binary Search
 		int a =0; 
         int b=(int) Math.sqrt(c);
 		while (a <= b)
         {
-			 int sum = a * a + b * b;
+			int sum = a * a + b * b;
 			if (c == sum)
-            {
 				return true;
-			}
 			else if(sum < c)
-            {
 				a++;
-			}
 			else 
-            {
-				b --;
-			}
+				b--;
 		}
 		return false;
 	}
+		
+	/**************Method 2*//////////////////////
+	
+//     public boolean judgeSquareSum(int c)
+//     {
+		
+// 		int a =0; 
+//         int b=(int) Math.sqrt(c);
+// 		while (a <= b)
+//         {
+// 			 int sum = a * a + b * b;
+// 			if (c == sum)
+//             {
+// 				return true;
+// 			}
+// 			else if(sum < c)
+//             {
+// 				a++;
+// 			}
+// 			else 
+//             {
+// 				b --;
+// 			}
+// 		}
+// 		return false;
+// 	}
 }
