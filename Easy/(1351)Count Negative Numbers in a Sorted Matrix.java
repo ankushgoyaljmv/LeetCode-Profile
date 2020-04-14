0e9cc480bda2f30c6c0d5e25e49c6edd3fdda637
Ankush Goyal <54682973+ashgrininja@users.noreply.github.com>
2020-04-14 21:13:38 +0530

    Code Optimized using binary search

--- Easy/(1351)Count Negative Numbers in a Sorted Matrix.java
+++ Easy/(1351)Count Negative Numbers in a Sorted Matrix.java
@@ -1,46 +1,73 @@
 //Problem:1351
 //https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix
-class Solution 
+class Solution
+
 {
-    
-    
- //************Optimised version 1:*************************//   
-//     public int countNegatives(int[][] grid) 
-//     {
-//          int end=grid[0].length-1;
-//         int count=0;
-        
-//         for(int i=0;i<grid.length;i++)
-//         {
-//             int temp=end;
-//             for(int j=end; j>=0; j--)
-//             {
-//                 if(grid[i][j]<0)
-//                     count++;
-                      
-//                 else break;
-//             }
-//         }
-//         return count;
-            
-//     }
-    
-    
-    
-    
+    /***************Using Binary Search*******************/
+
+    public int countNegatives(int[][] grid) 
+    {
+        int count = 0;
+        // taking one row at a time
+        for (int[] row: grid) {
+            count += binary(row);
+        }
+        return count;
+    }
+
+    int binary(int[] row) {
+        int start = 0, end = row.length;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (row[mid] < 0)
+                end = mid;
+            else
+                start = mid + 1;
+        }
+        return row.length - start;
+    }
+
+
+
+
+
+    //************Optimised version 1:*************************//   
+    //     public int countNegatives(int[][] grid) 
+    //     {
+    //          int end=grid[0].length-1;
+    //         int count=0;
+
+    //         for(int i=0;i<grid.length;i++)
+    //         {
+    //             int temp=end;
+    //             for(int j=end; j>=0; j--)
+    //             {
+    //                 if(grid[i][j]<0)
+    //                     count++;
+
+    //                 else break;
+    //             }
+    //         }
+    //         return count;
+
+    //     }
+
+
+
+
     //************************8Brute Force****************************8
-//     public int countNegatives(int[][] grid) 
-//     {
-//         int count=0;
-//         for(int i=0;i<grid.length;i++)
-//         {
-//             for(int j=0;j<grid[0].length;j++)
-//             {
-//                 if(grid[i][j]<0)
-//                     count++;
-//             }
-//         }
-//         return count;
-            
-//     }
+    //     public int countNegatives(int[][] grid) 
+    //     {
+    //         int count=0;
+    //         for(int i=0;i<grid.length;i++)
+    //         {
+    //             for(int j=0;j<grid[0].length;j++)
+    //             {
+    //                 if(grid[i][j]<0)
+    //                     count++;
+    //             }
+    //         }
+    //         return count;
+
+    //     }
 }
