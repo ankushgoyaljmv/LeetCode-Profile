04fd8921c7ba40512a0f3cfddbc81baa8080e50c
Ankush Goyal <54682973+ashgrininja@users.noreply.github.com>
2020-04-17 04:15:57 +0530

    COde added for 1394(using array:O(n).

--- Easy/(1394)Find Lucky Integer in an Array.java
+++ Easy/(1394)Find Lucky Integer in an Array.java
@@ -2,22 +2,33 @@
 //https://leetcode.com/problems/find-lucky-integer-in-an-array/
 
 class Solution {
+    
     public int findLucky(int[] arr) {
         int freq[] = freqCounter(arr);
-        int tempmax = 0;
-        int max = 0;
+        int res = -1;
         for (int f = 1; f < freq.length; f++) {
             if (freq[f] == f)
-                tempmax = f;
+                res = Math.max(f, res);
 
-            if (tempmax >= max)
-                max = tempmax;
         }
-        if (max == 0)
-            return -1;
-        else
-            return max;
+        return res;
     }
+//     public int findLucky(int[] arr) {
+//         int freq[] = freqCounter(arr);
+//         int tempmax = 0;
+//         int max = 0;
+//         for (int f = 1; f < freq.length; f++) {
+//             if (freq[f] == f)
+//                 tempmax = f;
+
+//             if (tempmax >= max)
+//                 max = tempmax;
+//         }
+//         if (max == 0)
+//             return -1;
+//         else
+//             return max;
+//     }
     public int[] freqCounter(int[] arr) {
         int[] freq = new int[500];
         for (int i = 0; i < arr.length; i++) {
