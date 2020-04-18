8df7695bd1e12c8b32f4bd37f31946ccf23986e1
Ankush Goyal <54682973+ashgrininja@users.noreply.github.com>
2020-04-18 20:20:28 +0530

    Extra Space code added

---
+++ Easy/(905)Sort Array By Parity.java
@@ -0,0 +1,24 @@
+class Solution {
+/***********Extra Space****************/
+
+    //     public int[] sortArrayByParity(int[] A) {
+    //         int res[]=new int[A.length];
+    //         int start=0;
+    //         int end=A.length-1;
+    //         for(int i=0;i<A.length;i++)
+    //         {
+    //             if(A[i]%2==0)
+    //             {
+    //               res[start]=A[i]; 
+    //               start++;
+    //             }
+    //             else
+    //             {
+    //               res[end]=A[i];
+    //                end--; 
+    //             }
+    //         }
+    //         return res;
+
+    //     }
+}
