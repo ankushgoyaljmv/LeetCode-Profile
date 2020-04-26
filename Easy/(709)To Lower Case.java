70406fb8c6eff38e0d7de1b1c1931daf97031cfc
Ankush Goyal <54682973+ashgrininja@users.noreply.github.com>
2020-04-27 00:59:14 +0530

    code updated

--- Easy/(709)To Lower Case.java
+++ Easy/(709)To Lower Case.java
@@ -1,8 +1,24 @@
+//Problem:
+//https://leetcode.com/problems/to-lower-case
+
 class Solution {
+    
+    //Using toCharArray
     public String toLowerCase(String str) {
         
-        return str.toLowerCase();
-        
-        
+        char[] arr = str.toCharArray();
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] >= 65 && arr[i] <= 90)
+				arr[i] += 32;
+		}
+		return new String(arr);
     }
 }
+
+//     public String toLowerCase(String str) {
+        
+//         return str.toLowerCase();
+        
+        
+//     }
+// }
